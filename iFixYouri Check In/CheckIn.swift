b499a00a60bc0de13b8a0d10c98ce9f4bbb7ad33
Sam Elder <astro@Sams-MacBook-Air.local>
2017-10-22 16:44:01 -0400

    init commit

---
+++ iFixYouri Check In/CheckIn.swift
@@ -0,0 +1,9 @@
+//
+//  CheckIn.swift
+//  iFixYouri Check In
+//
+//  Created by Sam Elder on 10/22/17.
+//  Copyright © 2017 Sam Elder. All rights reserved.
+//
+
+import Foundation
---
+++ iFixYouri Check In/constants.swift
@@ -0,0 +1,16 @@
+//
+//  arch.swift
+//  iFixYouri Check In
+//
+//  Created by Sam Elder on 10/22/17.
+//  Copyright © 2017 Sam Elder. All rights reserved.
+//
+
+import Foundation
+
+struct api {
+    
+    //API URIs
+    static let placeCheckIn = "http://localhost:4200/api/checkin/create"
+    
+}
