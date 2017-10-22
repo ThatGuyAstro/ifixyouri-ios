c2e12ccd6c8745c2d2ede07c30be0eca9d31d4f1
Sam Elder <astro@Sams-MacBook-Air.local>
2017-10-22 11:21:40 -0400

    Initial Commit

---
+++ iFixYouri Check In/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iFixYouri Check In
+//
+//  Created by Sam Elder on 10/22/17.
+//  Copyright © 2017 Sam Elder. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
